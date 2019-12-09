2b8532427cf93a776c19d357c4b3f97b7cf2913e
alff0x1f <alff0x1f@gmail.com>
2019-12-09 06:46:46 +0300

    show list records on index

--- phonebook/book/views.py
+++ phonebook/book/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from book.models import PhoneRecord
 
 
 def index(request):
-    return render(request, 'base.html', {'some_text': 'it works!'})
+    records = PhoneRecord.objects.all().order_by('name')
+    return render(request, 'index.html', {'records': records})
+
 
 def about(request):
     return render(request, 'about.html')
