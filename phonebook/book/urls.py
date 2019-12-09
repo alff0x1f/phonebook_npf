443fa53643a1800f554dee56d26a49c1e19d549f
alff0x1f <alff0x1f@gmail.com>
2019-12-09 07:21:04 +0300

    implement record delete

--- phonebook/book/urls.py
+++ phonebook/book/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('delete/<int:record_id>/', views.delete, name='delete'),
     path('about', views.about, name='about'),
 ]
--- phonebook/book/views.py
+++ phonebook/book/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from book.models import PhoneRecord
 
 
 def index(request):
     records = PhoneRecord.objects.all().order_by('name')
     return render(request, 'index.html', {'records': records})
+
+
+def delete(request, record_id):
+    if request.method == 'POST':
+        if 'record_id' in request.POST and int(request.POST['record_id']) == record_id:
+            try:
+                PhoneRecord.objects.get(id=record_id).delete()
+            except PhoneRecord.DoesNotExist:
+                raise Http404("File not found.")
+            else:
+                return HttpResponseRedirect("/")
+
+    try:
+        record = PhoneRecord.objects.get(id=record_id)
+    except PhoneRecord.DoesNotExist:
+        raise Http404("File not found.")
+    return render(request, 'delete.html', {'record': record})
 
 
 def about(request):
